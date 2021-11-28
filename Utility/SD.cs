0bc9cce648e9bde3cdc039990f25e8e94845a3e6
Nikhil Patel <90627433+W0776129@users.noreply.github.com>
2021-11-28 15:50:20 -0500

    1.4.2 Utility project modified and updated

---
+++ Utility/SD.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NikhilBooks.Utility
+{
+    public static class SD
+    {
+    }
+}
